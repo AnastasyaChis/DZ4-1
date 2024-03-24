acdb023c391a36c14923b6e29e53ed9824236174
Anastasia Chistyakova <kozlova27as@gmail.com>
2024-03-24 20:54:50 +0300

    Format code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
     public int calculate(int price) {
         int bonus = 20;
         int result;
-        result = price/bonus;
+        result = price / bonus;
         return result;
     }
 }
